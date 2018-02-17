0d8f6d5523c24d60b5e3dbfdeb3eced7259e2ffe
Alexis Koss <alexis30@uw.edu>
2018-02-16 17:25:02 -0800

    add legend for waterfront

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
-var margin = {top: 20, right: 20, bottom: 20, left: 80},
-    width = 950 - margin.left - margin.right,
-    height = 550 - margin.top - margin.bottom;
+var dataset; //the full dataset
+var patt = new RegExp("all");
+
+//var color = d3.scale.category10();
+
+var color = d3.scale.linear().domain([0,1])
+        .range(['#e18d5f', '#0ac7af']);
+
+
+var width = 750;
+var height = 450;
+var margin = {top: 90, right: 20, bottom: 20, left: 80};
+    var w = 950 - margin.left - margin.right;
+    var h = 550 - margin.top - margin.bottom;
 
 var x = d3.scale.linear()
     .range([0, width]);
 
 var y = d3.scale.linear()
     .range([height, 0]);
-
-var color = d3.scale.category10();
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -18,12 +27,24 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
-tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d.price; });
+tip = d3.tip().attr('class', 'd3-tip').html(function(d) { 
+    if(d.waterfront == 1) {
+        d.waterfront = "yes";
+     } else if(d.waterfront == 0) {
+         d.waterfront = "no";
+     } 
+     return "Price: $" + d.price + "<br/>" + "Sqft: " + d.sqft_living + "<br/>" +  "Bedrooms: " + d.bedrooms + "<br/>" + "Bathrooms: " + d.bathrooms + "<br/>" + "Waterfront: " + d.waterfront + "<br/>" + "Renovated: " + d.yr_renovated; });
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var chart = d3.select(".chart")
+    .attr("width", w + margin.left + margin.right)
+    .attr("height", h + margin.top + margin.bottom+15)
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     svg.call(tip);
@@ -36,10 +57,12 @@ d3.csv("house_prices.csv", function(error, data) {
     d.sqft_living = +d.sqft_living;
   });
 
+  dataset = data;
+
   x.domain(d3.extent(data, function(d) { return d.sqft_living; })).nice();
   y.domain(d3.extent(data, function(d) { return d.price; })).nice();
 
-  svg.append("g")
+  chart.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
@@ -50,7 +73,7 @@ d3.csv("house_prices.csv", function(error, data) {
       .style("text-anchor", "end")
       .text("Square Feet");
 
-  svg.append("g")
+  chart.append("g")
       .attr("class", "y axis")
       .call(yAxis)
     .append("text")
@@ -59,19 +82,64 @@ d3.csv("house_prices.csv", function(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Price")
+      .text("Sale Price")
 
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", function(d) { return x(d.sqft_living); })
-      .attr("cy", function(d) { return y(d.price); })
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
+  drawVis(dataset);
 });
 
+
+//creates the data visualization
+function drawVis() {
+    var circle = chart.selectAll(".dot")
+        .data(dataset);
+
+    circle
+        .attr("cx", function(d) { return x(d.sqft_living); })
+        .attr("cy", function(d) { return y(d.price); })
+            .style("fill", function(d, i) { return color(d.waterfront) })
+           
+    circle.exit().remove();
+
+    circle.enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 4.5)
+        .attr("cx", function(d) { return x(d.sqft_living); })
+        .attr("cy", function(d) { return y(d.price); })
+            .style("fill", function(d, i) { return color(d.waterfront) })
+            .style("opacity", 0.8)
+            .style("stroke", "#363636")
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
+
+}
+
+//creates legend for the graph
+var legend = chart.selectAll(".legend")
+.data(color.domain())
+.enter().append("g")
+.attr("class", "legend")
+.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+legend.append("rect")
+.attr("x", width - 18)
+.attr("width", 18)
+.attr("height", 18)
+.style("fill", color);
+
+legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) { 
+            if(d == 0) {
+                return "no waterfront"
+            } else {
+                return "has waterfront"
+            }
+        });
+
+//for creation of zipcode drop menu
 d3.csv("house_zipcodes.csv", function(error, data) {
     var select = d3.select("#zipCodeFilter")
       .append("select")
